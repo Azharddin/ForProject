37c9e54ae546d264d6880dacc6162aa977fe3a58
Azharddin <masterhac65@gmail.com>
2023-10-03 10:29:39 +0530

    changed

--- FindCount.java
+++ FindCount.java
@@ -3,8 +3,8 @@ package com.onesoft.forloop1;
 public class FindCount {
 	public static void main(String[]args) {
 		int count=0;
-		for(int i=0;i<=5;i++) {
-			count=count+1;
+		for(int i=0;i<=10;i++) {
+			count=count+i;
 		}
 		System.out.println(count);
 	}
